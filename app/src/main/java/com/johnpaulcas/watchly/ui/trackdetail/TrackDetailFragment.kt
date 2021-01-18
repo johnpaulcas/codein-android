08485d6600749d743fc4d36d861c9a3a1efbb40d
John Paul Cas <casjp.app@gmail.com>
2021-01-18 23:01:05 +0800

    🎨 Setup Navigation Graph

---
+++ app/src/main/java/com/johnpaulcas/watchly/ui/trackdetail/TrackDetailFragment.kt
@@ -0,0 +1,17 @@
+package com.johnpaulcas.watchly.ui.trackdetail
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.johnpaulcas.watchly.R
+
+/**
+ * Created by johnpaulcas on 18/01/2021.
+ */
+class TrackDetailFragment: Fragment(R.layout.fragment_track_detail) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
