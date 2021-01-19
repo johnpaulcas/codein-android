f3a5f3adbd5a856ca39888ab70e4fa3b05e34431
John Paul Cas <casjp.app@gmail.com>
2021-01-19 22:23:42 +0800

    ♻️ Add ColumnInfo anotation

--- app/src/main/java/com/johnpaulcas/watchly/persistence/database/Track.kt
+++ app/src/main/java/com/johnpaulcas/watchly/persistence/database/Track.kt
@@ -1,17 +1,27 @@
 package com.johnpaulcas.watchly.persistence.database
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "track_table")
 data class Track(
     @PrimaryKey
     val trackId: Int,
+
+    @ColumnInfo(name = "artwork_url")
     val artworkUrl100: String,
-    val collectionId: Int,
-    val collectionName: String,
+
+    @ColumnInfo(name = "track_name")
     val trackName: String,
+
+    @ColumnInfo(name = "genre")
     val primaryGenreName: String,
+
+    @ColumnInfo(name = "price")
     val trackPrice: Double,
+
+    @ColumnInfo(name = "description")
     val longDescription: String
 )
